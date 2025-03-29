e8f30c1833fe12a962035e490f26605e586e1fda
EashanVytla <Eashan.Vytla@gmail.com>
2025-03-29 00:20:58 -0400

    Adds main thread and forking framework

--- buffer.py
+++ buffer.py
@@ -1,14 +1,15 @@
 import multiprocessing as mp
 from collections import deque
+import numpy as np
 
 class Buffer:
     def __init__(self, capacity):
         self.queue = deque(maxlen=capacity)
         self.lock = mp.Lock()
 
-    def add(self):
+    def add(self, image: np.ndarray):
         with self.lock:
-            # Add image to queue
+            self.queue(image)
 
     def get_images(self):
         converted = []
